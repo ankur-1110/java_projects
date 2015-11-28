215c761b29248ee7278148e0270704ec20ce4ce9
Ankur Kumar <specialankur@gmail.com>
2015-11-28 09:05:26 +0530

    Test Files

---
+++ PrintName.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class PrintName{
+	public static void main(String s[]){
+
+		int a;
+		float b;
+		String s1;
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a string");
+		s1 = in.nextLine();
+		System.out.println("You entered string "+s1);
+		//System.out.println("Hello");		
+	}
+}
+
